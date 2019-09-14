2c1213b822c7d3b9c05c079fa4463845db6c81cc
Steven Anaya <stvanayar@gmail.com>
2019-09-13 20:47:26 -0500

    initial commit

---
+++ build/precache-manifest.28ea0baafd9f12d51e3b173738efd68a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93852525c717b3f993bde1cdeeb7c8d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31187cf3114f8b146b43",
+    "url": "/static/css/main.8be89c4b.chunk.css"
+  },
+  {
+    "revision": "c33ac989c2801703f117",
+    "url": "/static/js/2.0fd5193d.chunk.js"
+  },
+  {
+    "revision": "31187cf3114f8b146b43",
+    "url": "/static/js/main.785b6fb8.chunk.js"
+  },
+  {
+    "revision": "da66e40ecfec561bf3d0",
+    "url": "/static/js/runtime~main.ab8a0ebd.js"
+  }
+]);
